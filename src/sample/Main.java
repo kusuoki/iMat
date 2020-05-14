5bff70f565d949c4a5d24b46d6ac6083e605ccf3
Kusu <nanmu.w4@gmail.com>
2020-05-14 12:49:06 +0200

    fixed tidigareköp controller

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,17 +2,16 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
+import se.chalmers.cse.dat216.project.IMatDataHandler;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("mainpage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("tidigarekop.fxml"));
         primaryStage.setTitle("iMat");
         primaryStage.setScene(new Scene(root, 1277, 750));
         //Fundera på att fixa fullscreen
