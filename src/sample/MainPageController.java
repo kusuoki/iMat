581f6b4ef865d9c855e5ee003ef44328c964d40f
eliajo <eliascarlsward@gmail.com>
2020-05-14 10:45:24 +0200

    Added ability to switch FXML

--- src/sample/MainPageController.java
+++ src/sample/MainPageController.java
@@ -3,10 +3,12 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
 import se.chalmers.cse.dat216.project.*;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -15,6 +17,9 @@ public class MainPageController implements Initializable, ShoppingCartListener {
     private final Model model = Model.getInstance();
     @FXML
     TextField searchField;
+    //AnchorPane som ligger som grund till allt i MainPage
+    @FXML
+    AnchorPane mainAnchor;
 
     //Används för att sätta denna till kontroller för mainpage.fxml
     @Override
@@ -29,21 +34,31 @@ public class MainPageController implements Initializable, ShoppingCartListener {
 
     //När man klickar på listor
     @FXML
-    public void onListsClick() {
+    public void onListsClick(ActionEvent event) throws IOException {
+        AnchorPane list = FXMLLoader.load(getClass().getResource("listor.fxml"));
+        mainAnchor.getChildren().setAll(list);
+
+
     }
 
     //När man klickar på kundservice och hjälp
     @FXML
-    public void onCustomerServiceAndHelp() {
+    public void onCustomerServiceAndHelpClick(ActionEvent event) throws  IOException {
+        AnchorPane service = FXMLLoader.load(getClass().getResource("kundservice.fxml"));
+        mainAnchor.getChildren().setAll(service);
     }
 
     //När man trycker på kontoinställningar
     @FXML
-    public void onAccountSettings() {
+    public void onAccountSettingsClick(ActionEvent event) throws IOException {
+        AnchorPane account = FXMLLoader.load(getClass().getResource("kontoinställningar.fxml"));
+        mainAnchor.getChildren().setAll(account);
     }
     //När man trycker på betala
     @FXML
-    public void onPaymentButton() {
+    public void onPaymentButton(ActionEvent event) throws IOException {
+        AnchorPane payment = FXMLLoader.load(getClass().getResource("betalsida.fxml"));
+        mainAnchor.getChildren().setAll(payment);
     }
 
     //När man trycker på sökknappen
