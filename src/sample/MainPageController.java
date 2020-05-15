3664afd24fe374a5258fe5cebe9b3239692ec6b4
Paint19 <hanna@schaff.se>
2020-05-15 11:45:17 +0200

    Menyns design är interaktiv som den bör vara. Har ej kopplat mot undermenyer dock.

--- src/sample/MainPageController.java
+++ src/sample/MainPageController.java
@@ -1,20 +1,22 @@
 package sample;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-<<<<<<< HEAD
 import javafx.scene.layout.Pane;
-=======
 import javafx.scene.layout.FlowPane;
->>>>>>> 3b6493ff49c1dfcb6bb79ba9e4fc9adb8decf825
 import se.chalmers.cse.dat216.project.*;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+
 
 public class MainPageController implements Initializable, ShoppingCartListener {
     private final Model model = Model.getInstance();
@@ -23,13 +25,39 @@ public class MainPageController implements Initializable, ShoppingCartListener {
     @FXML
     FlowPane flowPaneMainPage;
 
+    @FXML Pane paneIndicatorEbjudanden;
+    @FXML Pane paneIndicatorFavoriter;
+    @FXML Pane paneIndicatorBaljvaxter;
+    @FXML Pane paneIndicatorSotsaker;
+    @FXML Pane paneIndicatorDrycker;
+    @FXML Pane paneIndicatorFisk;
+    @FXML Pane paneIndicatorFrukt;
+    @FXML Pane paneIndicatorKott;
+    @FXML Pane paneIndicatorMejeri;
+    @FXML Pane paneIndicatorSkafferi;
+
+    @FXML javafx.scene.control.Button buttonErbjudanden;
+    @FXML javafx.scene.control.Button buttonFavoriter;
+    @FXML javafx.scene.control.Button buttonBaljvaxter;
+    @FXML javafx.scene.control.Button buttonSotsaker;
+    @FXML javafx.scene.control.Button buttonDrycker;
+    @FXML javafx.scene.control.Button buttonFisk;
+    @FXML javafx.scene.control.Button buttonFrukt;
+    @FXML javafx.scene.control.Button buttonKott;
+    @FXML javafx.scene.control.Button buttonMejeri;
+    @FXML javafx.scene.control.Button buttonSkafferi;
+
+    ArrayList<Pane> menuIndicators = new ArrayList<Pane>();
+    ArrayList<javafx.scene.control.Button> menuButtons= new ArrayList<javafx.scene.control.Button>();
+
+
     //Används för att sätta denna till kontroller för mainpage.fxml
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-<<<<<<< HEAD
-=======
+        initMenuItems();
 
-        flowPaneMainPage.getChildren().add(new ListItem(model.getInstance().getProduct(10), model));
+
+       /* flowPaneMainPage.getChildren().add(new ListItem(model.getInstance().getProduct(10), model));
         flowPaneMainPage.getChildren().add(new ListItem(model.getInstance().getProduct(11), model));
         flowPaneMainPage.getChildren().add(new ListItem(model.getInstance().getProduct(12), model));
         flowPaneMainPage.getChildren().add(new ListItem(model.getInstance().getProduct(13), model));
@@ -39,9 +67,45 @@ public class MainPageController implements Initializable, ShoppingCartListener {
         flowPaneMainPage.getChildren().add(new ListItem(model.getInstance().getProduct(17), model));
         flowPaneMainPage.getChildren().add(new ListItem(model.getInstance().getProduct(18), model));
         flowPaneMainPage.getChildren().add(new ListItem(model.getInstance().getProduct(19), model));
-        flowPaneMainPage.getChildren().add(new ListItem(model.getInstance().getProduct(9), model));
+        flowPaneMainPage.getChildren().add(new ListItem(model.getInstance().getProduct(9), model));*/
 
->>>>>>> 3b6493ff49c1dfcb6bb79ba9e4fc9adb8decf825
+    }
+
+    void initMenuItems(){
+        menuIndicators.add(paneIndicatorEbjudanden);
+        menuIndicators.add(paneIndicatorFavoriter);
+        menuIndicators.add(paneIndicatorBaljvaxter);
+        menuIndicators.add(paneIndicatorSotsaker);
+        menuIndicators.add(paneIndicatorDrycker);
+        menuIndicators.add(paneIndicatorFisk);
+        menuIndicators.add(paneIndicatorFrukt);
+        menuIndicators.add(paneIndicatorKott);
+        menuIndicators.add(paneIndicatorMejeri);
+        menuIndicators.add(paneIndicatorSkafferi);
+
+        menuButtons.add(buttonErbjudanden);
+        menuButtons.add(buttonFavoriter);
+        menuButtons.add(buttonBaljvaxter);
+        menuButtons.add(buttonSotsaker);
+        menuButtons.add(buttonDrycker);
+        menuButtons.add(buttonFisk);
+        menuButtons.add(buttonFrukt);
+        menuButtons.add(buttonKott);
+        menuButtons.add(buttonMejeri);
+        menuButtons.add(buttonSkafferi);
+
+        for (Pane p : menuIndicators) {
+            p.toBack();
+        }
+
+        System.out.println(menuButtons);
+
+        for(javafx.scene.control.Button b : menuButtons){
+            b.setOnAction((event)->{
+                onMenuClick(b);
+            });
+        }
+
     }
 
     //När man klickar på tidigare köp
@@ -75,10 +139,37 @@ public class MainPageController implements Initializable, ShoppingCartListener {
         updateProductList(searchList);
     }
 
+
+
     //När man klickar på menyn
+
+
     @FXML
-    public void onMenyClick() {
+    public void onMenuClick(javafx.scene.control.Button b) {
+        for(javafx.scene.control.Button button : menuButtons) //Clears any button that may've been clicked before
+        {
+            if(button != b) {
+                if (button.getStyleClass().toString().equals("menuButtonClicked")) {
+                    button.getStyleClass().clear();
+                    button.getStyleClass().add("menuButton");
+                    //Här ska man också ta bort den gamla undermenyn. Kolla på att göra en ihopsättning av indicators, knappar och hela nya menyvyn
+                }
+            }
+
+        }
+            System.out.println(b.getStyleClass());
+            if (b.getStyleClass().toString().equals("menuButtonClicked")) {
+                b.getStyleClass().clear();
+                b.getStyleClass().add("menuButton");
+            } else {
+                b.getStyleClass().clear();
+                b.getStyleClass().add("menuButtonClicked");
+            }
+
     }
+
+
+
 
     //När man hoovrar över menyn
     @FXML
