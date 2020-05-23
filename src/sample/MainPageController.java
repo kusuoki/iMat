0cf50478730d29c0b5cb7b76ddfa6df9ff0bbd69
TJOCKochFIN <viktorhjoh@gmail.com>
2020-05-23 10:30:43 +0200

    tooltip

--- src/sample/MainPageController.java
+++ src/sample/MainPageController.java
@@ -893,19 +893,19 @@ public class MainPageController implements Initializable, ShoppingCartListener {
             varukorgItems.add(item);
         }
 
-<<<<<<< HEAD
-            if (model.getShoppingCart().getItems().size() == 0) {
-                buttonBetala.setGraphic(whiteBetala);
-            } else {
-                buttonBetala.setGraphic(blackBetala);
-            }
-=======
+
+        if (model.getShoppingCart().getItems().size() == 0) {
+            buttonBetala.setGraphic(whiteBetala);
+        } else {
+            buttonBetala.setGraphic(blackBetala);
+        }
+
         flowPaneVarukorg.getChildren().clear();
         for (VarukorgItem item : varukorgItems) {
             item.updateThisItem();
             flowPaneVarukorg.getChildren().add(item);
         }
->>>>>>> c2f0a0d24242991bf294396e954bc2d9122b3935
+
 
         if (model.getShoppingCart().getItems().size() == 0) {
             buttonBetala.setStyle("-fx-background-color: #A0A0A0; -fx-text-fill:white;");
